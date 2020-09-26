9b3d1d757499786c0dbc18b3f930403e14375326
Aleksey <fduchunru@gmail.com>
2020-09-26 04:59:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ return "Happy holidays!"
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
+
+function happyCustomHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
